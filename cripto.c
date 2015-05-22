577109e8a8597b11dcf5f742b356befcc5ed9cee
contiver <cristianontivero@gmail.com>
2015-05-22 12:43:07 -0300

    polinomios modelados, agregadas funciones para los primeros 2 pasos del 'sharing phase'

--- cripto.c
+++ cripto.c
@@ -1,15 +1,17 @@
-#include <unistd.h>
+#include <math.h>
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define BMP_HEADER_SIZE        14
 #define DIB_HEADER_SIZE_OFFSET 14
 #define FILESIZE_OFFSET        2	
 #define WIDTH_OFFSET           18
 #define HEIGHT_OFFSET          22
+#define MOD_PRIME              251
 
 typedef enum {
 	BITMAPCOREHEADER   = 12,
@@ -34,17 +36,17 @@ typedef struct {
 
 /* 40 bytes BITMAPINFOHEADER */
 typedef struct {
-  uint32_t size;        /* the size of this header (40 bytes) */
-  uint32_t width;       /* the bitmap width in pixels */
-  uint32_t height;      /* the bitmap height in pixels */
-  uint16_t nplanes;     /* number of color planes used; Must set to 1 */
-  uint16_t depth;       /* bpp number. Usually: 1, 4, 8, 16, 24 or 32 */
-  uint32_t compression; /* compression method used */
-  uint32_t rawbmpsize;  /* size of the raw bitmap (pixel) data */
-  uint32_t hres;        /* horizontal resolution (pixel per meter) */
-  uint32_t vres;        /* vertical resolution (pixel per meter) */
-  uint32_t ncolors;     /* number of colors in the palette. 0 means 2^n */
-  uint32_t nimpcolors;  /* number of important colors used, usually ignored */
+	uint32_t size;        /* the size of this header (40 bytes) */
+	uint32_t width;       /* the bitmap width in pixels */
+	uint32_t height;      /* the bitmap height in pixels */
+	uint16_t nplanes;     /* number of color planes used; Must set to 1 */
+	uint16_t depth;       /* bpp number. Usually: 1, 4, 8, 16, 24 or 32 */
+	uint32_t compression; /* compression method used */
+	uint32_t rawbmpsize;  /* size of the raw bitmap (pixel) data */
+	uint32_t hres;        /* horizontal resolution (pixel per meter) */
+	uint32_t vres;        /* vertical resolution (pixel per meter) */
+	uint32_t ncolors;     /* number of colors in the palette. 0 means 2^n */
+	uint32_t nimpcolors;  /* number of important colors used, usually ignored */
 } DIBheader;
 
 typedef struct {
@@ -54,20 +56,36 @@ typedef struct {
 	uint8_t *data;       /* image pixels */
 } Bitmap;
 
+typedef struct{
+	int *coeff;
+	int degree;
+} Polynomial;
+
 /* prototypes */ 
-static void     randomize(int num);
-static double   randnormalize(void);
-static long     randint(long int max);
-static uint32_t bmpfilesize(FILE *fp);
-static uint32_t bmpfilewidth(FILE *fp);
-static uint32_t bmpfileheight(FILE *fp);
-static uint32_t get32bitsfromheader(FILE *fp, char offset);
-static void     freebitmap(Bitmap *bp);
-static void     die(const char *errstr, ...);
-static void     *xmalloc(size_t size);
-static FILE     *xfopen(const char *filename, const char *mode);
-static void     xfclose(FILE *fp);
-static void     bmptofile(Bitmap *bp, const char *filename);
+static void       die(const char *errstr, ...);
+static void       *xmalloc(size_t size);
+static FILE       *xfopen(const char *filename, const char *mode);
+static void       xfclose(FILE *fp);
+static void       usage(void);
+static long       randint(long int max);
+static double     randnormalize(void);
+static Polynomial *newpolynomial(int *coeff, int degree);
+static void       freepolynomial(Polynomial *p);
+static int        evaluate(Polynomial *p, int value);
+static uint32_t   get32bitsfromheader(FILE *fp, char offset);
+static uint32_t   bmpfilesize(FILE *fp);
+static uint32_t   bmpfilewidth(FILE *fp);
+static uint32_t   bmpfileheight(FILE *fp);
+static uint32_t   bmpfiledibheadersize(FILE *fp);
+static void       freebitmap(Bitmap *bp);
+static Bitmap     *bmpfromfile(char *filename);
+static void       bmptofile(Bitmap *bp, const char *filename);
+static int        bmpimagesize(Bitmap *bp);
+static int        bmppalettesize(Bitmap *bp);
+
+/* globals */
+static char *arg0;            /* program name */
+static int 	sharedpixels = 0; /* shared pixels */
 
 void
 die(const char *errstr, ...) {
@@ -105,9 +123,9 @@ xfclose (FILE *fp){
 		die("couldn't close file\n");
 }
 
-void 
-randomize(int num){
-	srand((int) num);
+void
+usage(void){
+	die("usage: %s -(d|r) -secret image -k number [-n number|-dir directory]\n", arg0);
 }
 
 double 
@@ -120,16 +138,47 @@ randint(long int max){
 	return (long) (randnormalize()*(max+1)); /*returns num in [0,max]*/
 } 
 
+Polynomial *
+newpolynomial(int *coeff, int degree){
+	Polynomial *p = xmalloc(sizeof(*p));
+	p->coeff      = coeff;
+	p->degree     = degree;
+
+	return p;
+}
+
+void
+freepolynomial(Polynomial *p){
+	free(p->coeff);
+	free(p);
+}
+
+int
+evaluate(Polynomial *p, int value){
+	int i; 
+	long ret = 0;
+
+	for(i = 0; i < p->degree; i++)
+		ret += p->coeff[i] * powl(value,i);
+
+	return ret % MOD_PRIME;
+}
+
 uint32_t
 get32bitsfromheader(FILE *fp, char offset){
 	uint32_t value;
 	uint32_t pos = ftell(fp);
 
 	fseek(fp, offset, SEEK_SET);
-	fread(&value, 1, 4, fp);
+	fread(&value, 4, 1, fp);
 	fseek(fp, pos, SEEK_SET);
 
 	return value;
+}
+
+uint32_t
+bmpfilesize(FILE *fp){
+	return get32bitsfromheader(fp, FILESIZE_OFFSET);
 }
 
 uint32_t
@@ -143,18 +192,12 @@ bmpfileheight(FILE *fp){
 }
 
 uint32_t
-bmpfilesize(FILE *fp){
-	return get32bitsfromheader(fp, FILESIZE_OFFSET);
-}
-
-uint32_t
 bmpfiledibheadersize(FILE *fp){
 	uint32_t size = get32bitsfromheader(fp, DIB_HEADER_SIZE_OFFSET);
 	if(size != BITMAPINFOHEADER)
 		die("unsupported dib header format\n");
 	return size;
 }
-
 
 void
 freebitmap(Bitmap *bp){
@@ -201,14 +244,14 @@ bmpfromfile(char *filename){
 	dibheaderdebug(bp);
 
 	/* read color palette */
-	int palettesize = bp->bmpheader.offset - (BMP_HEADER_SIZE + bp->dibheader.size);
+	int palettesize = bmppalettesize(bp);
 	bp->palette = xmalloc(palettesize);
 	i = 0;
 	while(i < palettesize)
 		i += fread(bp->palette + i, 1, palettesize, fp);
 
 	/* read pixel data */
-	int imagesize = bp->bmpheader.size - bp->bmpheader.offset;
+	int imagesize = bmpimagesize(bp);
 	bp->data = xmalloc(imagesize);
 	i = 0;
 	while(i < imagesize)
@@ -218,29 +261,48 @@ bmpfromfile(char *filename){
 	return bp;
 }
 
+int
+bmpimagesize(Bitmap *bp){
+	return bp->bmpheader.size - bp->bmpheader.offset;
+}
+
+int
+bmppalettesize(Bitmap *bp){
+	return bp->bmpheader.offset - (BMP_HEADER_SIZE + bp->dibheader.size);
+}
+
 void
 bmptofile(Bitmap *bp, const char *filename){
 	FILE *fp = xfopen(filename, "w");
-	int palettesize = bp->bmpheader.offset - (BMP_HEADER_SIZE + bp->dibheader.size);
-	int imagesize = bp->bmpheader.size - bp->bmpheader.offset;
 
 	fwrite(&bp->bmpheader, sizeof(bp->bmpheader), 1, fp);
 	fwrite(&bp->dibheader, sizeof(bp->dibheader), 1, fp);
-	fwrite(bp->palette, palettesize, 1, fp);
-	fwrite(bp->data, imagesize, 1, fp);
+	fwrite(bp->palette, bmppalettesize(bp), 1, fp);
+	fwrite(bp->data, bmpimagesize(bp), 1, fp);
 	xfclose(fp);
 }
 
 void
-changebmppixel(FILE *fp, int pixel, char bit){
-	uint8_t byte;
+truncategrayscale(Bitmap *bp){
+	int i;
+	for(i = 0; i < 1024; i++)
+		if(bp->palette[i] > 250)
+			bp->palette[i] = 250;
+}
 
-	// TODO
-	//fseek(fp, HEIGHT_OFFSET, SEEK_SET);
-	fread(&byte, 1, 1, fp);
-	byte &= bit;
-	fseek(fp, -1, SEEK_CUR);
-	fwrite(&byte, 1, 1, fp);
+/* Durstenfeld algorithm */
+void
+permutepixels(Bitmap *bp){
+	int i, j, temp;
+	uint8_t *p = bp->data;
+	srand(10); /* FIXME preguntar sobre la "key" de permutación! */
+
+	for(i = bmpimagesize(bp)-1; i > 1; i--){
+		j = randint(i);
+		temp = p[j];
+		p[j] = p[i];
+		p[i] = temp;
+	}
 }
 
 int
@@ -249,6 +311,8 @@ main(int argc, char *argv[]){
 	char *arg;
 	FILE *fp;
 
+	arg0 = argv[0];
+
 	/*
 	   if(strcmp("-secret", arg) == 0){
 	   }
@@ -256,6 +320,8 @@ main(int argc, char *argv[]){
 
 	filename = argv[1];
 	Bitmap *bp = bmpfromfile(filename);
+	truncategrayscale(bp);
+	permutepixels(bp);
 	bmptofile(bp, "output.bmp");
 	freebitmap(bp);
 }
